52797c40934838986f746afc68e933f3f1632582
vankhang49 <dvkhang49@gmail.com>
2024-06-16 09:30:57 +0700

    KhangDV add

---
+++ src/pages/Dashboard/PersonInfo/PersonInfo.js
@@ -0,0 +1,79 @@
+import {HeaderDashboard} from "../../../components/header/HeaderDashboard";
+import {SidebarDashboard} from "../../../components/sidebar/SidebarDashboard";
+import {useState} from "react";
+
+
+export function PersonInfo() {
+
+    const [isShowSidebar, setIsShowSidebar] = useState(false);
+
+    const callbackFunction = (childData) => {
+        setIsShowSidebar(childData)
+    }
+
+    return (
+        <div className="app-container">
+            <HeaderDashboard parentCallback={callbackFunction}></HeaderDashboard>
+            <div className={"content-wrapper"}>
+                <SidebarDashboard showSidebar={isShowSidebar}></SidebarDashboard>
+                <div className="app-content">
+                    <div className="content-body">
+
+                        <div className="content-element">
+                            <div className="flex-content">
+                                <table className="person-info">
+                                    <tbody>
+                                    <tr>
+                                        <th>Tên nhân viên</th>
+                                        <td>Nguyễn Văn A</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Mã nhân viên</th>
+                                        <td>0123456789</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Ngày sinh</th>
+                                        <td>01/02/0123</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Địa chỉ</th>
+                                        <td>Hoà Khánh, Đà Nẵng</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Số điện thoại</th>
+                                        <td>0123456789</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                                <form className="form-operation">
+                                    <div className="old-password form-element">
+                                        <label>Mật khẩu cũ: </label>
+                                        <input type="text" name="oldPassword"/>
+                                        <p className="validate-error">Mật khẩu không đúng!!</p>
+                                    </div>
+                                    <div className="new-password form-element">
+                                        <label>Mật khẩu mới: </label>
+                                        <input type="text" name="newPassword"/>
+                                        <p className="validate-error">Mật khẩu không đúng định dạng!!</p>
+                                    </div>
+                                    <div className="confirm-password form-element">
+                                        <label>Nhập lại mật khẩu: </label>
+                                        <input type="text" name="confirmPassword"/>
+                                        <p className="validate-error">Mật khẩu không trùng khớp!</p>
+                                    </div>
+                                    <div className="form-element">
+                                        <button type="submit" className="btn-submit">
+                                            Đổi mật khẩu
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
---
+++ src/routes/routes.js
@@ -0,0 +1,22 @@
+import { Routes, Route} from 'react-router-dom';
+import HomePage from '../pages/Home/HomePage';
+import NotFound from '../pages/NotFound/NotFound';
+import LoginPage from '../pages/Login/LoginPage';
+import NewsPage from '../pages/News/NewsPage';
+import {Dashboard} from "../pages/Dashboard/Dashboard";
+import {PersonInfo} from "../pages/Dashboard/PersonInfo/PersonInfo";
+
+function Approutes(props) {
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/news" element={<NewsPage />} />
+            <Route path="/person-info" element={<PersonInfo />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
+}
+
+export default Approutes;
