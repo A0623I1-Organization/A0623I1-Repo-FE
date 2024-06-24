1ab83062d77df7fda97917795b8d53fb9b56dca8
levanquy0101 <quyle.01012002@gmail.com>
2024-06-25 01:22:01 +0700

    QuyLV

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,13 @@
 import HomePage from '../pages/Home/HomePage';
-import NotFound from '../pages/NotFound/NotFound';
+import NotFound from '../pages/ErrorPage/NotFound';
 import LoginPage from '../pages/Login/LoginPage';
 import NewsPage from '../pages/News/NewsPage';
 import { Dashboard } from '../pages/Dashboard/Dashboard';
 import { PersonInfo } from '../pages/Dashboard/PersonInfo/PersonInfo';
 import { EmployeeList } from "../pages/Dashboard/EmployeeManagement/EmployeeList";
 import { EmployeeCreate } from "../pages/Dashboard/EmployeeManagement/EmployeeCreate";
+import CustomerCreate from '../pages/Dashboard/Customer/CustomerCreate';
+import CustomerUpdate from '../pages/Dashboard/Customer/CustomerUpdate';
 
 const routes = [
   {
@@ -28,18 +30,28 @@ const routes = [
     private: true,
   },
   {
-    path: '/dashboard/employee-list',
+    path: '/dashboard/employee',
     element: <EmployeeList />,
     private: true,
   },
   {
-    path: '/dashboard/employee-create',
+    path: '/dashboard/employee/create',
     element: <EmployeeCreate />,
     private: true,
   },
   {
-    path: '/dashboard/employee-create/:id',
+    path: '/dashboard/employee/create/:id',
     element: <EmployeeCreate />,
+    private: true,
+  },
+  {
+    path: '/dashboard/customer/create',
+    element: <CustomerCreate />,
+    private: true,
+  },
+  {
+    path: '/dashboard/customer/update',
+    element: <CustomerUpdate />,
     private: true,
   },
   {
