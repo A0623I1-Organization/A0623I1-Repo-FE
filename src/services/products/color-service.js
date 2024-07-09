74f15ad88530432bff5627865b697ce8d44d1c47
vankhang49 <dvkhang49@gmail.com>
2024-07-09 15:35:41 +0700

    KhangDV push FE

--- src/services/products/color-service.js
+++ src/services/products/color-service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getAllColor =async ()=>{
     try {
-        let temp = await axios.get(`http://192.168.1.50:8080/api/color`)
+        let temp = await axios.get(`http://localhost:8080/api/color`)
         return temp.data;
     }catch (e)
     {
